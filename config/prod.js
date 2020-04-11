ba9ddca39b023de14119da6582cd738601e5e7f8
wanderer <tthe.wandererr@gmail.com>
2020-04-11 21:20:13 +0200

    Make production variables entirely env dependent

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,10 @@
 module.exports = {
     cookieKey: process.env.COOKIE_KEY,
     mongoURI : process.env.MONGO_URI,
-    discordOauthURL : 'https://discordapp.com/api/oauth2/authorize?client_id=692886126045692025&redirect_uri=https%3A%2F%2Fbot.wanderer.lol%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify',
-    discordOauthRedirectURI: 'https://bot.wanderer.lol/auth/discord/callback',
+    discordOauthURL : process.env.DISCORD_OAUTH_URL,
+    discordOauthRedirectURI: process.env.DISCORD_OAUTH_CALLBACK_URL,
     discordClientID : process.env.DISCORD_CLIENT_ID,
     discordClientSecret : process.env.DISCORD_CLIENT_SECRET,
-    botAPIKey: 'ESGAihjksehdquiawdWEFREt433daDcadfvad',
-    botAPIDomain: 'https://bot.wanderer.lol/'
+    botAPIKey: process.env.BOT_API_KEY,
+    botAPIDomain: process.env.BOT_API_DOMAIN
 };
